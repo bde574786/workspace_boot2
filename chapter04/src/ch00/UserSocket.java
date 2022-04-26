62290325498b8f8b1a10cc2d3866d461696b12c8
bde574786 <kimnaye09@naver.com>
2022-04-26 17:48:37 +0900

    Socket

---
+++ chapter04/src/ch00/UserSocket.java
@@ -0,0 +1,63 @@
+package ch00;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
+public class UserSocket extends Thread {
+
+	private ServerFile mContext;
+	private Socket socket;
+
+	private BufferedReader bufferedReader;
+	private BufferedWriter bufferedWriter;
+
+	public UserSocket(ServerFile mContext, Socket socket) {
+		this.mContext = mContext;
+		this.socket = socket;
+
+		try {
+			bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+		public void run() {
+			new Thread(new Runnable() {
+				
+				@Override
+				public void run() {
+					while (true) {
+						try {
+							String msg = bufferedReader.readLine();
+							System.out.println("서버 메시지 " + msg);
+							mContext.broadcast(msg);
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+					}
+				}
+			}).start();
+
+		}
+
+	public void sendMessage(String msg) {
+		try {
+			bufferedWriter.write(msg + '\n');
+			bufferedWriter.flush();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+}
