fc89aca7b784cd690a93cb33b4e2e568d52452fc
bde574786 <kimnaye09@naver.com>
2022-05-04 17:51:28 +0900

    DB연결

---
+++ db_connect/src/ch02/MemberDto.java
@@ -0,0 +1,21 @@
+package ch02;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class MemberDto {
+
+	@NonNull
+	private String memberId;
+	private String memberName;
+	private String memberAddress;
+
+}
